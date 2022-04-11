a4d5998ef8019af6dbc0ce4d51f769a2ef4e7ef0
Kristian Picon <kristian@waffl.in>
2022-04-10 21:52:44 -0400

    Add some rudimentary error handling

--- js/emdr-express-join-view.js
+++ js/emdr-express-join-view.js
@@ -11,14 +11,23 @@ export default class EmdrExpressJoinView {
         this.peer = new Peer();
         location.hash = this.id;
         this._bindEventListeners();
+        this.view = document.querySelector('.js-join-view');
     }
 
     _bindEventListeners() {
         this.peer.on('error', err => {
             console.error(err);
+            this.peer = null;
+            //remove hash
+            history.replaceState("", document.title, location.pathname + location.search);
+            location.reload();
         });
         this.peer.on('open', () => {
+            this.view.textContent = 'Connecting...';
             const conn = this.peer.connect(this.id);
+            conn.on('open', () => {
+                this.view.textContent = 'Connected.';
+            });
             conn.on('data', data => {
                 console.log(data);
                 if (data.event === 'play') {
